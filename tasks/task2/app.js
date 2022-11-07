bfc71a3e67eec7337c613fd6a56bb333db37cdbb
Rufat Guliyev <47758460+rftglyv@users.noreply.github.com>
2022-11-08 01:10:44 +0400

    Task Done Partially

--- tasks/task2/app.js
+++ tasks/task2/app.js
@@ -24,3 +24,20 @@ function DupRem(){
     document.getElementById("result-text1").innerHTML = "Result : " + remDupl(arr)
 }
 document.getElementById("result-btn1").onclick = function() {DupRem()};
+
+let rowContainer = document.querySelector(".board-row")
+let rowContainerWidth = rowContainer.clientWidth
+
+for (let i=1; i<=8; i++) {
+  let box = document.createElement("div");
+  box.innerHTML = i
+  box.setAttribute("class", "box d-flex justify-content-center align-items-center");
+  rowContainer.appendChild(box);
+  if(i%2==0){
+    box.classList.add("bg-dark");
+    box.setAttribute('style', `width: ${rowContainerWidth/8}px; height: ${rowContainerWidth/8}px; color: white;`)
+  }else{
+    box.classList.add("bg-light");
+    box.setAttribute('style', `width: ${rowContainerWidth/8}px; height: ${rowContainerWidth/8}px; color: black;`)
+  }
+}

a80100885374e7b587d67605297779f49a0c99ea
Rufat Guliyev <47758460+rftglyv@users.noreply.github.com>
2022-11-10 23:19:03 +0400

    v0.2

--- counter/scr1.js
+++ counter/scr1.js
@@ -1,37 +1,78 @@
-let monthField = document.getElementById('month');
-let dayField = document.getElementById('day');
-let hourField = document.getElementById('hour');
-let minuteField = document.getElementById('minute');
-let secondField = document.getElementById('second');
-
-let interval;
-
 const second = 1000;
 const minute = second * 60;
 const hour = minute * 60;
 const day = hour * 24;
 const month = day * 30;
 
-var eventDay = new Date();
-eventDay = new Date("November 17, 2022 00:00:00");
+let exams = [
+  {
+    name: "Morpholgy",
+    examDate: "November 17, 2022 00:00:00"
+  },
+  {
+    name: "CODH",
+    examDate: "November 18, 2022 00:00:00"
+  },
+  {
+    name: "English",
+    examDate: "November 19, 2022 00:00:00"
+  },
+  {
+    name: "Turkish",
+    examDate: "November 20, 2022 00:00:00"
+  }
+]
 
-const countDownFn = () => 
-{
+const listGroup = document.querySelector(".list-group");
+
+for (i in exams){
+  let examTemplate = `<li class="list-group-item d-flex justify-content-center">
+  <div class="d-flex flex-column align-items-center">
+    <p class="h3">${exams[i].name}</p>
+    <div class="date-container justify-content-around">
+      <h2 id="month-${i}">00</h2>
+      <p>:</p>
+      <h2 id="day-${i}">00</h2>
+      <p>:</p>
+      <h2 id="hour-${i}">00</h2>
+      <p>:</p>
+      <h2 id="minute-${i}">00</h2>
+      <p>:</p>
+      <h2 id="second-${i}">00</h2>
+    </div>
+  </div>
+</li>`;
+	listGroup.innerHTML += examTemplate;
+}
+
+function main(){
+  for (i in exams){
+    monthField = document.getElementById(`month-${i}`);
+    dayField = document.getElementById(`day-${i}`);
+    hourField = document.getElementById(`hour-${i}`);
+    minuteField = document.getElementById(`minute-${i}`);
+    secondField = document.getElementById(`second-${i}`);
+    counter(i)
+  }
+}
+
+function counter(i){
+  eventDay = new Date();
+  eventDay = new Date(exams[i].examDate);
   const today = new Date();
   const timeSpan = eventDay - today;
 
-    const months = Math.floor(timeSpan / month);
-    const days = Math.floor(timeSpan / day);
-    const hours = Math.floor((timeSpan % day) / hour);
-    const minutes = Math.floor((timeSpan % hour) / minute);
-    const seconds = Math.floor((timeSpan % minute) / second);
+  const months = Math.floor(timeSpan / month);
+  const days = Math.floor((timeSpan % month) / day);
+  const hours = Math.floor((timeSpan % day) / hour);
+  const minutes = Math.floor((timeSpan % hour) / minute);
+  const seconds = Math.floor((timeSpan % minute) / second);
 
-    monthField.innerHTML = months;
-    dayField.innerHTML = days;
-    hourField.innerHTML = hours;
-    minuteField.innerHTML = minutes;
-    secondField.innerHTML = seconds;
-
+  monthField.innerHTML = months;
+  dayField.innerHTML = days;
+  hourField.innerHTML = hours;
+  minuteField.innerHTML = minutes;
+  secondField.innerHTML = seconds;
 }
 
-interval = setInterval(countDownFn, second);
+setInterval(function(){main()}, 1000);

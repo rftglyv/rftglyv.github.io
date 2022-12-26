67eb925c743eae66c7e6a95239d0f53393bf266a
Rufat Guliyev <47758460+rftglyv@users.noreply.github.com>
2022-12-26 21:37:54 +0400

    Added custom cursor

--- raft/app.js
+++ raft/app.js
@@ -53,3 +53,13 @@ const countDownFn = () =>
 }
 
 interval = setInterval(countDownFn, second);
+
+const cursor = document.getElementById('cursor');
+
+const moveCursor = (e)=> {
+  const mouseY = e.clientY;
+  const mouseX = e.clientX;
+  cursor.style.transform = `translate3d(calc(${e.clientX}px - 75%), calc(${e.clientY}px - 75%), 0)`;
+}
+
+window.addEventListener('mousemove', moveCursor)

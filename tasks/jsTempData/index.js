898f4392c2817a2e03e20e6c8503cb9eab182b76
Rufat Guliyev <47758460+rftglyv@users.noreply.github.com>
2023-05-26 03:03:49 +0400

    Task Done

---
+++ tasks/jsTempData/index.js
@@ -0,0 +1,69 @@
+let mainMenu = document.getElementById("mainMenu"),
+    subMenu = document.getElementById("subMenu"),
+    addNews = document.getElementById("addNews"),
+    catTags = document.getElementById("catTags");
+
+let categories = [
+    { name: "Ölkə", sub: ["Cəmiyyət", "Hava", "Kriminal"] },
+    { name: "İqtisadiyyat", sub: ["Sərgilər", "Banklar"] },
+    { name: "İdman", sub: [] }
+]
+
+for (let i = 0; i < categories.length; i++) {
+    let cat = categories[i].name,
+        elem = document.createElement("option");
+    elem.textContent = cat;
+    elem.value = i;
+    mainMenu.appendChild(elem);
+}
+
+mainMenu.addEventListener("change", () => {
+    subMenu.innerHTML = `<option selected="">Alt kateqoriya seçin</option>`;
+    let cat = categories[mainMenu.value].sub;
+    if (cat.length == 0) {
+        subMenu.innerHTML = `<option selected="">Kateqoriyaya uyğun alt kateqoriya yoxdur</option>`;
+        subMenu.disabled = true;
+    } else {
+        subMenu.disabled = false;
+        for (let i = 0; i < cat.length; i++) {
+            let sub = cat[i],
+                elem = document.createElement("option");
+            elem.textContent = sub;
+            elem.value = i;
+            subMenu.appendChild(elem);
+        }
+    }
+})
+
+addNews.addEventListener("click", () => {
+    if (mainMenu.selectedIndex && subMenu.selectedIndex !== 0) {
+        let tag = categories[mainMenu.value].sub == 0 ? mainMenu.options[mainMenu.selectedIndex].text : subMenu.options[subMenu.selectedIndex].text
+        catTag = `
+        <button onclick="this.parentNode.removeChild(this)" class="d-flex align-items-center justify-content-between me-2 p-2 fs-6 alert alert-primary category">
+            <span name="${tag}">${tag}</span> 
+            <i class="fa-solid fa-xmark fs-4 x-btn"></i>
+        </button>
+        `
+        btnTag = document.getElementsByName(tag)
+        if (btnTag.length == 0) {
+            catTags.innerHTML += catTag;
+        } else {
+            btnTag[0].parentElement.classList.add("alert-added")
+            removeAlert = () => { setTimeout(() => { btnTag[0].parentElement.classList.remove("alert-added") }, 1000) }
+            removeAlert()
+        }
+    } else {
+        if (mainMenu.selectedIndex == 0) {
+            mainMenu.classList.add("alert-added")
+        } else {
+            subMenu.classList.add("alert-added")
+        }
+        removeAlert = () => {
+            setTimeout(() => {
+                mainMenu.classList.remove("alert-added")
+                subMenu.classList.remove("alert-added")
+            }, 1000)
+        }
+        removeAlert()
+    }
+})
